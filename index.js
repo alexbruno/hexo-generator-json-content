67f31aeb1e733e1fc5f9bf2e2082be83b2fb7d04
Alex Bruno <webmaster@alexbruno.net.br>
2015-10-01 21:01:31 +0000

    Bugfix posts array

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ function hexo_generator_json_content(locals) {
     pages: []
   };
   
-  locals.posts.sort('-date').each(function (post) {
-    json.posts.push(post);
-  });
-  
   locals.pages.each(function (page) {
     json.pages.push(page);
+  });
+  
+  locals.posts.sort('-date').each(function (post) {
+    var tags = [], cats = [];
+    
+    post.tags.each(function (tag) {
+      tags.push(tag);
+    });
+    
+    post.categories.each(function (cat) {
+      cats.push(cat);
+    });
+    
+    post.tags = tags;
+    post.categories = cats;
+    
+    json.posts.push(post);
   });
   
   return hexo.route.set('content.json', JSON.stringify(json));
